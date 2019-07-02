656e57ff309839e087786ece1d8a7a1b4e9d0f86
Chase Mortensen <C_Mortensen@live.com>
2019-07-02 15:40:15 -0700

    Added smiley images

---
+++ images.smileys.ts
@@ -0,0 +1,8 @@
+namespace images.smileys {
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const happy = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const love = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const sad = image.ofBuffer(hex``);
+}
